1e3fe41fa5ea2003234065dc63a7500090f5c25f
Andrey Adamovich <andrey@aestasit.com>
2022-03-15 19:39:34 +0200

    fixed linter warnings

--- Organizer/tests/conftest.py
+++ Organizer/tests/conftest.py
@@ -11,13 +11,15 @@ def app():
     with app.app_context():
         yield app
 
+
 @pytest.fixture()
 def client(app: Flask):
     return app.test_client()
 
+
 @pytest.fixture()
 def prepare_data(app):
-    task = Task(id=777, task="to be deleted")    
+    task = Task(id=777, task="to be deleted")
     db.session.add(task)
     db.session.commit()
     print(task.id)
