a6f7a4cbd75b7101fadb489b6d37d15954089c06
FelesFurva <kitten.solar@gmail.com>
2022-04-01 15:01:43 +0300

    s

--- Organizer/project/routes/auth.py
+++ Organizer/project/routes/auth.py
@@ -1,6 +1,6 @@
 from functools import wraps
 
-from flask import Blueprint, Request
+from flask import Flask, Blueprint, request
 from flask_login import login_user, logout_user
 from project import db
 from project.models import User
@@ -9,54 +9,50 @@ from werkzeug.security import check_password_hash, generate_password_hash
 auth = Blueprint("auth", __name__)
 
 
-def login_required(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if User.query.get(id) is None:
-            return {"message": "user id is none"}
-        return f(*args, **kwargs)
+#def login_required(f):
+#    @wraps(f)
+#    def decorated_function(*args, **kwargs):
+#        if User.query.get(id) is None:
+#            return {"message": "user id is none"}
+#        return f(*args, **kwargs)
 
-    return decorated_function
+#    return decorated_function
 
 
-@auth.route("/signup", methods=["GET", "POST"])
+@auth.route("/signup", methods=["POST"])
 def signup():
-    if Request.method == "POST":
 
-        email=Request.json["email"]
-        username=Request.json["username"]
-        password=Request.json["password"]
-        
-        if not Request.json["username"] or not Request.json["password"] or not Request.json["email"]:
-            return {"message": "Please fill out all fields"}
-        else:
-            user = User.query.filter_by(email=email).first()
-            if user:
-                return {"message": "User already exists"}
-            new_user = User(username=username, password_hash=generate_password_hash(password, method="sha256"), email=email)
 
-        db.session.add(new_user)
+        user = User(username=request.json["username"], password_hash=generate_password_hash(request.json["password"], method="sha256"), email=request.json["email"])
+
+        db.session.add(user)
         db.session.commit()
-        return {"id": user.id, "message": "Account successfully created"}, 201
-    return {"message": "Under construction"}, 404
+        return {"id": user.id}, 201
 
 
-@auth.route("/login", methods=["GET", "POST"])
+@auth.route("/login", methods=["POST"])
 def login():
-    if Request.method == "POST":
-        email = Request.json["email"]
-        password = Request.json["password"]
+    
+        password = request.json["password"]
 
-        user = User.query.filter_by(email=email).firts()
+        user = User.query.filter_by(email=request.json["email"]).firts()
         if not user or not check_password_hash(user.password_hash, password):
             return {"message": "Invalid username/password combination"}
         login_user(user)
         return {"message": "Login successful"}, 200
-    return {"message": "Under construction"}, 404
 
+@auth.route("/user/<id>", method=["PUT"])
+def edit_user(id):
+
+    user = User.query.get_or_404(id)
+    user.user = request.json.get("username", user.username)
+
+    db.session.commit()
+
+    return {"id": user.id, "username": user.username}, 201
 
 @auth.route("/logout", methods=["GET"])
-@login_required
+#@login_required
 def logout():
     logout_user()
     return {"message": "Logout successful"}, 200
--- Organizer/tests/functional/test_user_signup.py
+++ Organizer/tests/functional/test_user_signup.py
@@ -2,11 +2,6 @@ from numbers import Number
 
 
 def test_user_create_post(client):
-    response = client.post("/signup", json={"Account succesfully created"})
+    response = client.post("/signup")
     assert 201 == response.status_code
     assert isinstance(response.json["id"], Number)
-
-
-def test_user_create_get(client):
-    response = client.get("/signup")
-    assert 404 == response.status_code
